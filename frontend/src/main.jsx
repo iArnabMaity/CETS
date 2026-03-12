8190e740b52df32261e37e7e81cf7d92e5ab762f
Arnab Maity <arnabmaity822@gmail.com>
2026-03-12 14:03:45 +0530

    Enabled Vercel Speed Insights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// 1. Import the Analytics helper
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    {/* 2. Add the component here */}
+    <SpeedInsights />
+  </React.StrictMode>,
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
