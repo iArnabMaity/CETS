33b1bfb2bf5f7ea53234d90b70ea76e9a02d4783
Arnab Maity <arnabmaity822@gmail.com>
2026-03-12 14:14:08 +0530

    Enabled Vercel Speed Insights

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,6 @@ import AuthPage from './components/AuthPage';
 import EmployeeDashboard from './components/EmployeeDashboard';
 import EmployerDashboard from './components/EmployerDashboard';
 import AdminDashboard from './components/AdminDashboard';
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-// 1. Import the Analytics helper
-import { SpeedInsights } from "@vercel/speed-insights/react"
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    <SpeedInsights />
-  </React.StrictMode>,
-)
 
 const SESSION_TIMEOUT_MS = 30 * 60 * 1000; // 30 minutes
 
