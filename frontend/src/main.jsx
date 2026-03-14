3226e982ada63ad4d2e9baaecc345830e7c96759
Arnab Maity <arnabmaity822@gmail.com>
2026-03-14 10:04:34 +0530

    Enabled Vercel Analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-// Use the /react import for your Vite project
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
+import { Analytics } from "@vercel/analytics/react"
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <SpeedInsights />
+    {/* 2. Add the Analytics component here */}
+    <Analytics />
+  </React.StrictMode>,
+)
+
+import { SpeedInsights } from "@vercel/speed-insights/react"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <SpeedInsights />
   </React.StrictMode>,
 )
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
